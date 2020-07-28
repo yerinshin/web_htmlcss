cbc475a8b80ad3ae4d8ebe278e7e7e5eb8202bf1
yerinshin <user-email>
2020-07-28 22:11:09 +0900

    using jquery

--- colors.js
+++ colors.js
@@ -1,19 +1,23 @@
-    function setColor(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i = i + 1;
-   }
+var Links = {
+    setColor:function(color){
+//     var alist = document.querySelectorAll('a');
+//     var i = 0;
+//     while(i < alist.length){
+//       alist[i].style.color = color;
+//       i = i + 1;
+//    }
+    $('a').css('color', color);
   }
-
+}
    var Body = {
       setColor:function (color){
-        document.querySelector('body').style.color = color;
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
       },
       setBackgroundColor:function (color){
-        document.querySelector('body').style.backgroundColor = color;
-      }
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
+    }
    }
    function nightDayHandler(self){
       var target = document.querySelector('body');
@@ -21,12 +25,12 @@
         Body.setBackgroundColor('black');
         Body.setColor('white');
         self.value ='day';
-        setColor('powderblue');
+        Links.setColor('powderblue');
         }
       else {
       Body.setBackgroundColor('white');
       Body.setColor('black');
       self.value ='night';
-      setColor('blue');
+      Links.setColor('blue');
       }
    }
